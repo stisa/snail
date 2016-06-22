5842157d44c9e26230d528eb2774cd0ccf454a5d
Silvio T <stisa4@gmail.com>
2016-06-22 18:39:20 +0200

    blas works! l_1, dot working

--- nalg.nim
+++ nalg.nim
@@ -9,12 +9,16 @@ var m : array[2, array[2, float]] = [[2.0,1.0],[3.0,4.0]]
 var mm : Matrix[2,2] = matrix(m)
 
 var v : Vector[2] = vector([2.0,1.0])
+var vv : Vector[2] = vector([2.0,1.0])
+#[]
+shallowCopy(vv,v.data)
+var vvv: array[2, ptr float64]
+for i,j in v.data:
+    var tmp : float64
+    shallowCopy tmp, j
+    vvv[i] = addr tmp
 
-var vv = 0
-#var vp = addr vv
-var vp : array[2, ptr float64] 
-#=map(v.data, proc(x:float64):ptr float64= addr x)
-var rv : ref Vector[2] =v
+echo vvv[0][]
+]#
 
-
-echo vp[]
+echo v*vv
--- private/operations.nim
+++ private/operations.nim
@@ -1,14 +1,29 @@
 # transpose in place. Should file a bug with nim: single ' not allowed as operator
+# TODO: fix discard, this should be inlineable?
 proc `t`* [N:static[int]] (v: var Vector[N]): Vector[N] =
     if v.isCol: v.isCol=false else: v.isCol=true
-    return v 
 
 # transpose, copying. Should file a bug with nim: single ' not allowed as operator
 proc `t`* [N:static[int]] (v: Vector[N]): Vector[N] =
     result = v
     if result.isCol: result.isCol=false else: result.isCol=true
 
-template fp(v: ref Vector): array[ v.N, ptr float64] = v.data.mapIt(addr(it))
+template fp [N:static[int]](v: Vector[N]): ptr float64  = 
+    var vpf: array[N, float64]
+    shallowCopy vpf, v.data
+    cast[ptr float64](addr vpf[0])
 
-proc abss [N:static[int]] (v: Vector[N]): float64 =
-    result = asum(N, v.fp, 1)
+template fp [N,M:static[int]](m: Matrix[N,M]): ptr float64  = 
+    var mpf: array[N*M, float64]
+    shallowCopy vpf, m.data
+    cast[ptr float64](addr mpf[0])
+
+# take the sum of the abs values of the elements of a vector v
+proc l_1 [N:static[int]] (v: Vector[N]): auto =
+    asum(N, v.fp, 1)
+
+# Vector dot product
+proc dot [N:static[int]] (v: Vector[N], w: Vector[N]): auto =
+    dot(N, v.fp, 1, w.fp, 1)
+# shorthand to^^
+proc `*` [N:static[int]] (v: Vector[N], w: Vector[N]): auto {.inline.}= dot(v,w)
