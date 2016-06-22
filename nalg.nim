4b3b6ae469fc4ad57ec85603f71f070299215989
Silvio T <stisa4@gmail.com>
2016-06-22 14:39:16 +0200

    fix m.col(c) ; fix accessors

--- nalg.nim
+++ nalg.nim
@@ -8,7 +8,8 @@ var v :Vector[3] = Vector[3](data: [2.0,2.0,2.0], isCol:false)
 
 var m : Matrix[2,2] = Matrix[2,2]( data: [2.0,1.0,3.0,4.0])
 m[1,1]= 5.0
-echo m
-echo m.row(2)
+echo m.row(0)[1]
+echo m.col(0)
+#echo m[0,0]
 
 #SEEME: single column matrix accesors not working, out of bounds, need ifs?
--- private/types.nim
+++ private/types.nim
@@ -13,22 +13,27 @@ type
     Vector* [N:static[int]] = object
         data: array [N, float64]
         isCol: bool
-    
-    Vec* [N:static[int]] = object
-        data: array [N, float64]
 
     Array[N: static[int]] = array[N, float64] # hackhis, gives nicer code tho
 
+
+
+#[ Vector methods ]#
 proc vector* [N:static[int]] (arr: Array[N], isCol:bool = false): Vector[N] = 
     result.data = arr
     result.isCol = isCol
 
+proc len*[N: static[int]](v: Vector[N]): int = N
+
+proc `[]`*[ N : static[int] ](v: Vector[N], i:int): float64 = v.data[i]
+
+proc `[]=`*[ N : static[int] ](v: Vector[N], i:int, val: float64)= v.data[i] = val 
+
+#[ Matrix methods ]#
+
+# matrix([1-D arr]) = Matrix[N,M]
 proc matrix* [N:static[int],M:static[int]] (arr: Array[N]): Matrix[N,M] = 
     result.data = arr
-
-## These should probably go to their own file
-
-proc len*[N: static[int]](v: Vector[N]): int = N
 
 proc at[ N,M : static[int] ](m : Matrix[N,M], i, j: int): float64 = m.data[i * m.N + j] 
 
@@ -37,18 +42,15 @@ proc `[]`*[ N,M : static[int] ](m : Matrix[N,M], i, j: int): float64 = m.data[i 
 proc `[]=`*[ N,M : static[int] ](m : var Matrix[N,M], i, j: int, val: float64)= m.data[i * m.N + j] = val 
 
 #extract a row
-proc row *[ N,M : static[int] ](m : var Matrix[N,M], r: int) : Matrix[1,M]=
-    assert(r<N, "The matrix has less rows than the requested row index") 
-    var ind:int = 0
+proc row *[ N,M : static[int] ](m : var Matrix[N,M], r: int) : Vector[M]=
+    assert(r<N, "The matrix has less rows than the requested row index")
     for i in 0..M-1:
-        result[0,ind] = m.data[r*m.N+i]
-        ind+=1
-
-#extract s vol
-proc col *[ N,M : static[int] ](m : var Matrix[N,M], c: int) : Matrix[N,1]=
-    assert(c<M, "The matrix has less cols than the requested col index") 
-    var ind:int = c
-    for i in 0..N-1:  # uglyy
-        echo result[0,1]  
-        #result[i,0] = m.data[ind]
-        #ind+=M-1
+        result.data[i] = m.data[r*m.N+i]
+        
+#extract a col
+proc col *[ N,M : static[int] ](m : var Matrix[N,M], c: int) : Vector[N]=
+    assert(c<M, "The matrix has less cols than the requested col index")
+    result.isCol = true
+    for i in 0..N-1:
+        result.data[i] = m.data[i*m.N+c]
+       
