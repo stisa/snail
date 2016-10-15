16e3259f8649a8d7ef73ec3eda54161c360512fd
Silvio T <stisa4@gmail.com>
2016-10-15 22:00:00 +0200

    still ill-conditioned...

--- tests/ops.nim
+++ tests/ops.nim
@@ -1,4 +1,4 @@
-var A = matrix2D([[0.0,12,0],[4.0,0,0],[0.0,0,3]])
-var b = colVector(3,[2.0,1,1])
+var A = matrix2D([[0.0,5,6],[4.0,5,7],[9.0,2,3]])
+var b = colVector(3,[11.0,16,15])
 
 echo "sol->",solve(A,b)
