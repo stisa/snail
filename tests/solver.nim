36f171c7568af6de05d89c6de47ad29eebc4ec4e
Silvio T <stisa4@gmail.com>
2016-10-16 19:05:04 +0200

    add simple solver test

--- tests/solver.nim
+++ tests/solver.nim
@@ -7,5 +7,6 @@ import ../nalg
 
 var A = matrix2D([[0.0,5,6],[4.0,5,7],[9.0,2,3]])
 var b = colVector(3,[11.0,16,15])
-
-echo solve(A,b)
+#var x = colVector(3,[])
+#assert($solve(A,b)==
+assert( $solve(A,b) == "[+1.15|\n|+1.73|\n|+0.39]\n" )
