9934a9cc92e2123dae58d95dc7e0c95f43b6c450
Silvio T <stisa4@gmail.com>
2016-10-21 16:35:41 +0200

    finish up rewrite of matrix

--- snail/matrix.nim
+++ snail/matrix.nim
@@ -1,4 +1,4 @@
-from vector import RowVector,ColVector,Vector
+from vector import RowVector,ColVector,Vector,dot,`[]=`,`$`
 from strutils import formatFloat,FloatFormatMode
 type 
     #[
@@ -92,12 +92,123 @@ proc col *[N,M : static[int]](m : Matrix[N,M], c: int) : ColVector[N]=
   new result.data
   for i in 0..<N: result.data[i] = m.data[i*m.N+c]
 
+proc `t`* [N,M:static[int]](m: Matrix[N,M]): Matrix[M,N] =
+  new result.data
+  for i in m.low..m.high: result.data[][i] = m.data[][i]
+
+proc reshape* [N,M:static[int]](m: Matrix[N,M],U,V:static[int]): Matrix[U,V] =
+  assert(N*M==U*V, "Matrix and its reshaping have different number of elements")
+  new result.data
+  for i in m.low..m.high: result.data[][i] = m.data[][i]
+
+proc matMul* [N,M,V:static[int]](m: Matrix[N,M], w: Matrix[M,V]) :Matrix[N,V] =
+  new result.data
+  ## Naive matrix multiplication
+  for r in 0..<N: # iter on rows of m
+    for c in 0..<V: # on cols of w
+      result[r,c] = dot(m.row(r),w.col(c))
+
+proc `*`* [N,M,V:static[int]](m: Matrix[N,M],
+  w: Matrix[M,V]) :Matrix[N,V] {.inline.} = matMul(m,w)
+
+proc vecMatMul* [N,M:static[int]](m: Matrix[N,M], v: ColVector[M]) :ColVector[N] =
+  new result.data
+  for r in 0..<N: # iter on rows of m
+    result[r] = dot(m.row(r),v) 
+
+proc vecMatMul* [N,M:static[int]](v: RowVector[N],m: Matrix[N,M]) :RowVector[M] =
+  new result.data
+  for c in 0..<M: # iter on rows of m
+    result[c] = dot(v,m.col(c)) 
+
+proc `*`* [N,M:static[int]](m: Matrix[N,M],
+  v: ColVector[M]) :ColVector[N] {.inline.} = vecMatMul(m,v)
+
+proc `*`* [N,M:static[int]](v: RowVector[N],m: Matrix[N,M]) :RowVector[M] {.inline.} = vecMatMul(v,m)
+
+
+proc matAdd *[N,M:static[int]] (m,w: Matrix[N,M]): Matrix[N,M] = 
+    new result.data
+    for i,e in m.data[].pairs:
+      result.data[i] = e+w.data[i]
+# shorthand to^^
+proc `+` *[N,M:static[int]] (m: Matrix[N,M], w: Matrix[N,M]): Matrix[N,M]{.inline.}= matAdd(m,w)
+
+proc matSub *[N,M:static[int]] (m,w: Matrix[N,M]): Matrix[N,M] = 
+    new result.data
+    for i,e in m.data[].pairs:
+      result.data[i] = e-w.data[i]
+# shorthand to^^
+proc `-` *[N,M:static[int]] (m: Matrix[N,M], w: Matrix[N,M]): Matrix[N,M]{.inline.}= matSub(m,w)
+
+proc elMatMul *[N,M:static[int]] (val:float,m: Matrix[N,M]): Matrix[N,M] = 
+    new result.data
+    if val == 0.0: return # short circuit, the result is already 0 so no need to do it again
+    for i,e in m.data[].pairs:
+      result.data[i] = val*e
+# shorthand to^^
+proc `.*` *[N,M:static[int]] (val:float,m: Matrix[N,M]): Matrix[N,M]{.inline.}= elMatMul(val,m)
+
+proc elMatDiv *[N,M:static[int]] (m: Matrix[N,M],val:float): Matrix[N,M] = 
+    assert(val!=0.0, "Division by zero")
+    new result.data
+    for i,e in m.data[].pairs:
+      result.data[i] = e/val
+# shorthand to^^
+proc `./` *[N,M:static[int]] (m: Matrix[N,M],val:float): Matrix[N,M]{.inline.}= elMatDiv(m,val)
+
+proc elMatAdd *[N,M:static[int]] (m: Matrix[N,M],val:float): Matrix[N,M] = 
+    new result.data
+    for i,e in m.data[].pairs:
+      result.data[i] = e+val
+# shorthand to^^
+proc `.+` *[N,M:static[int]] (m: Matrix[N,M],val:float): Matrix[N,M]{.inline.}= elMatAdd(m,val)
+
+proc elMatSub *[N,M:static[int]] (m: Matrix[N,M],val:float): Matrix[N,M] = 
+    new result.data
+    for i,e in m.data[].pairs:
+      result.data[i] = e-val
+# shorthand to^^
+proc `.-` *[N,M:static[int]] (m: Matrix[N,M],val:float): Matrix[N,M]{.inline.}= elMatSub(m,val)
+
+proc subtractVecFromRow* [N,M:static[int]](A:var Matrix[N,M],row:int,vec:RowVector[M],times:float=1.0)=
+  assert(row<N)
+  for j in 0..<M: A[row,j] = A[row,j]-A[row,j]*times*vec[j]
+
+proc subtractVecFromCol* [N,M:static[int]](A:var Matrix[N,M],col:int,vec:ColVector[N],times:float=1.0)=
+  assert(col<M)
+  for j in 0..<N: A[j,col] = A[j,col]-A[j,col]*times*vec[j]
+
+proc divRowBy* [N,M:static[int]](A:var Matrix[N,M],row:int,val:float)=
+  assert(row<N)
+  for j in 0..<M: A[row,j] = A[row,j]/val
+
+proc divColBy* [N,M:static[int]](A:var Matrix[N,M],col:int,val:float)=
+  for j in 0..<N: A[j,col] = A[j,col]/val
+
+proc norm*[N,M:static[int]] (m: Matrix[N,M]): float =
+  for i in 0..<N:
+    var tmp = 0.0
+    for j in 0..<M:
+      tmp += abs(m[i,j])
+    echo tmp
+    if tmp>result: result = tmp
+
 when isMainModule:
   from vector import colVec,rowVec,`==`
   var m : Matrix[2,2] = matrix([[0.0,1],[1.0,0]])
   var w : Matrix[2,2] = [0.0,1,1,0].toMatrix(2,2)
+  var r : Matrix[2,2] = [1.0,0,0,1].toMatrix(2,2)
+  var cv: ColVector[2] = colVec([1.0,1])
+  var rv: RowVector[2] = rowVec([1.0,1.0])
   assert(m==w)
   assert(dims(m) == (2,2))
   assert(m.row(1) == rowVec([1.0,0]))
   assert(m.col(1) == colVec([1.0,0]))
-  echo m
+  
+  assert(m*cv==cv)
+  assert(rv*m==rv)
+
+  assert(m*w==r)
+  
+  #echo m*v
